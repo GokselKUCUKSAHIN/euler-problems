cc78c5c527b4c47337ef1bc2292a97b247c90cf9
Jellybeanci <gokselkucuksahin@gmail.com>
2021-03-21 00:29:54 +0300

    Problem 2 Solved.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
-
+import problems_1_10.Problem1;
+import problems_1_10.Problem2;
 
 public class Main
 {
 
   public static void main(String[] args)
   {
+    System.out.println(Problem1.eulerProblem1BruteForce());
+    System.out.println(Problem1.eulerProblem1Optimized());
+    System.out.println(Problem2.problem2());
   }
 }
