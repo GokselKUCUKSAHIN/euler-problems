a69a4431daf40b0196b0d15deb5bffb45c1e3c83
Jellybeanci <gokselkucuksahin@gmail.com>
2021-03-23 15:30:26 +0300

    Problem 7 documentation added.

--- src/problems_1_10/Problem7.java
+++ src/problems_1_10/Problem7.java
@@ -5,6 +5,13 @@ import java.util.Arrays;
 
 public class Problem7
 {
+  /*
+  By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+  What is the 10 001st prime number?
+
+  Answer = 104743
+  */
 
   private static int[] sieveOfEratothenes(int max)
   {
