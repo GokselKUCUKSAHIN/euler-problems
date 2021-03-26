11f45973265b31b84bf289837de614e06c6f9b7f
Jellybeanci <gokselkucuksahin@gmail.com>
2021-03-26 17:41:28 +0300

    Problem9 Optimized.

--- src/problems_1_10/Problem9.java
+++ src/problems_1_10/Problem9.java
@@ -2,6 +2,7 @@ package problems_1_10;
 
 public class Problem9
 {
+
   /*
   A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 
@@ -16,23 +17,23 @@ public class Problem9
   */
   public static int eulerProblem9()
   {
-    for (int c = 1; c <= 1000; c++)
-      for (int b = 0; b < c; b++)
-        for (int a = 0; a < b; a++)
+    for (short c = 335; c <= 1000; c++)
+      for (short b = 0; b < c; b++)
+        for (short a = 0; a < b; a++)
           if (sumRule(a, b, c) && pythagoreanRule(a, b, c))
           {
-            System.out.printf("a = %d, b = %d, c = %d\n", a, b, c);
+            // System.out.printf("a = %d, b = %d, c = %d\n", a, b, c);
             return a * b * c;
           }
     return 0;
   }
 
-  public static boolean sumRule(int a, int b, int c)
+  public static boolean sumRule(short a, short b, short c)
   {
     return 1000 == a + b + c;
   }
 
-  public static boolean pythagoreanRule(int a, int b, int c)
+  public static boolean pythagoreanRule(short a, short b, short c)
   {
     return Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2);
   }
