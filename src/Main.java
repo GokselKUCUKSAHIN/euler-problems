50b60bb67857960af1d2d8b16f31af10a5bca2d8
Jellybeanci <gokselkucuksahin@gmail.com>
2021-03-26 17:08:04 +0300

    Problem 9 solved.

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,16 @@ public class Main
 
   public static void main(String[] args)
   {
-    // System.out.println(Problem1.eulerProblem1BruteForce());
-    //System.out.println(Problem1.eulerProblem1Optimized());
-    //System.out.println(Problem2.eulerProblem2());
-    //System.out.println(Problem3.eulerProblem3());
+    // // System.out.println(Problem1.eulerProblem1BruteForce());
+    // System.out.println(Problem1.eulerProblem1Optimized());
+    // System.out.println(Problem2.eulerProblem2());
+    // System.out.println(Problem3.eulerProblem3());
     // System.out.println(Problem4.eulerProblem4());
     // // System.out.println(Problem5.eulerProblem5());
     // System.out.println(Problem5.eulerProblem5Optimized());
     // System.out.println(Problem6.eulerProblem6());
     // System.out.println(Problem7.eulerProblem7());
-    System.out.println(Problem8.eulerProblem8());
+    // System.out.println(Problem8.eulerProblem8());
+    System.out.println(Problem9.eulerProblem9());
   }
 }
