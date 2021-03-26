acc8a41877517becba73fa2bae609fadf7bcdd1e
Jellybeanci <gokselkucuksahin@gmail.com>
2021-03-26 17:09:30 +0300

    Text added.

--- src/problems_1_10/Problem9.java
+++ src/problems_1_10/Problem9.java
@@ -2,7 +2,18 @@ package problems_1_10;
 
 public class Problem9
 {
+  /*
+  A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 
+  a^2 + b^2 = c^2
+  For example, 3^2 + 4^22 = 9 + 16 = 25 = 5^2.
+
+  There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+  Find the product abc.
+
+  Answer = 31875000
+  a = 200, b = 375, c = 425
+  */
   public static int eulerProblem9()
   {
     for (int c = 1; c <= 1000; c++)
