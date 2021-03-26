347d744d214aa3a2388037c7293c35d185f87ec9
Jellybeanci <gokselkucuksahin@gmail.com>
2021-03-27 00:56:58 +0300

    Problem 10 answer and text added.

--- src/problems_1_10/Problem10.java
+++ src/problems_1_10/Problem10.java
@@ -5,6 +5,14 @@ import java.util.Arrays;
 
 public class Problem10
 {
+  /*
+  The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+  Find the sum of all the primes below two million.
+
+  Answer = 142913828922;
+   */
+
 
   private static int[] sieveOfEratothenes(int max)
   {
