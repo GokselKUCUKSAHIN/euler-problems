e19aa412a5dc7af2de297843209ff52f15af1f72
Jellybeanci <gokselkucuksahin@gmail.com>
2021-03-27 00:56:06 +0300

    Problem10 Solved.

---
+++ src/problems_1_10/Problem10.java
@@ -0,0 +1,28 @@
+package problems_1_10;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Problem10
+{
+
+  private static int[] sieveOfEratothenes(int max)
+  {
+    boolean[] isPrime = new boolean[max];
+    Arrays.fill(isPrime, true);
+    for (int p = 2; p * p < max - 2; p++)
+      if (isPrime[p - 2])
+        for (int i = p * p; i < max - 2; i += p)
+          isPrime[i - 2] = false;
+    ArrayList<Integer> primes = new ArrayList<>();
+    for (int p = 2; p < max - 2; p++)
+      if (isPrime[p - 2])
+        primes.add(p);
+    return primes.stream().mapToInt(Integer::intValue).toArray();
+  }
+
+  public static long eulerProblem10()
+  {
+    return Arrays.stream(sieveOfEratothenes(2_000_000)).asLongStream().sum();
+  }
+}
